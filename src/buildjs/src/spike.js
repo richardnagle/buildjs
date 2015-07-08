db2d82bb63e2f6ebf70f1540423853cc699c6e6e
Richard Nagle <rnagle@yahoo.com>
2015-07-08 08:02:45 +0100

    Selecting colour working in spike

--- src/buildjs/src/spike.js
+++ src/buildjs/src/spike.js
@@ -1,6 +1,8 @@
 ﻿export class Spike {
 
     constructor(){
+        this.selectedColour = this.makeColour(8,8,8);
+
         this.rows = new Array(16*16);
                 
         for (var b=0;b<16; b++) {
@@ -8,11 +10,30 @@
                 var swabs = new Array(16);
             
                 for(var r=0; r<16; r++){
-                    swabs[r] = (r*256*256*16)+(g*256*16)+(b*16);
+                    var c = this.makeColour(r*16,g*16,b*16);
+                    swabs[r] = new ColourViewModel(c, this);
                 }
             
                 this.rows[(b*16)+g] = swabs;
             }
         }
+    }
+
+    makeColour(r,g,b)
+    {
+        return (r*256*256)+(g*256)+b;
+    }
+}
+
+export class ColourViewModel
+{
+    constructor(c, parent){
+        this.value = c;
+        this.parent = parent;
+    }
+
+    colourClicked()
+    {
+        this.parent.selectedColour = this.value;
     }
 }
