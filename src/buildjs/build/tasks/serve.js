9169af708ee3dc5080ad0146045f28dd5411fa14
Richard Nagle <rnagle@yahoo.com>
2015-08-19 17:19:47 +0100

    Removed some of the ts support that didn't work Change gulp watch to proxy iisexpress with browsersync

--- src/buildjs/build/tasks/serve.js
+++ src/buildjs/build/tasks/serve.js
@@ -4,16 +4,24 @@ var browserSync = require('browser-sync');
 // this task utilizes the browsersync plugin
 // to create a dev server instance
 // at http://localhost:9000
-gulp.task('serve', ['build'], function(done) {
-  browserSync({
-    open: false,
-    port: 9000,
-    server: {
-      baseDir: ['.'],
-      middleware: function (req, res, next) {
-        res.setHeader('Access-Control-Allow-Origin', '*');
-        next();
-      }
-    }
-  }, done);
+//gulp.task('serve', ['build'], function(done) {
+//  browserSync({
+//    open: false,
+//    port: 9000,
+//    server: {
+//      baseDir: ['.'],
+//      middleware: function (req, res, next) {
+//        res.setHeader('Access-Control-Allow-Origin', '*');
+//        next();
+//      }
+//    }
+//  }, done);
+//});
+
+gulp.task('serve', ['build'], function (done) {
+    browserSync({
+        open: false,
+        proxy: 'localhost:49387',
+        port: 9000
+    }, done);
 });
